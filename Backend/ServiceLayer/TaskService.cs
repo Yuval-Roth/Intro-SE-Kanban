821aaa396ce07d5d93f4cb26ac067d6b1be9c4a0
kafire135 <kfirniss@post.bgu.ac.il>
2022-04-16 12:01:26 +0300

    added taskservice

--- Backend/ServiceLayer/TaskService.cs
+++ Backend/ServiceLayer/TaskService.cs
@@ -5,4 +5,14 @@ public class TaskService
 	public TaskService()
 	{
 	}
+
+	public Json getTitle () { }
+	public Json setTitle (string t) { }
+	public Json getDuedate () { }
+	public Json setDuedate (Date d) { }
+	public Json getDescription () { }
+	public Json setDescription (string d) { }
+	public Json getState () { }
+	public Json setState (enum e) { }
+
 }
