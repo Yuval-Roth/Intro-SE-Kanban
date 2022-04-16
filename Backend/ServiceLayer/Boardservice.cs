1203ba2b864ca6c1294a74211c36a13b6022b38b
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2022-04-16 12:23:58 +0300

    Updated BoardService.cs

--- Backend/ServiceLayer/Boardservice.cs
+++ Backend/ServiceLayer/Boardservice.cs
@@ -5,4 +5,21 @@ public class BoardService
 	public BoardService()
 	{
 	}
+	public json getTitle() 
+	{
+	}
+	public json getTasksByType(enum type){} 
+	{
+	}
+	public json addTask(string title, Date dueDate, string description) 
+	{
+	}
+	public json removeTask(string title)
+	{
+	}
+	public json advanceTask(string title)
+	{ 
+	}
+
+
 }
