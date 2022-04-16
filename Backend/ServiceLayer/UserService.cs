bd37100916dbe4421cd36dd4e8397536d29b543a
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2022-04-16 12:03:53 +0300

    added UserService

--- Backend/ServiceLayer/UserService.cs
+++ Backend/ServiceLayer/UserService.cs
@@ -5,4 +5,24 @@ public class UserService
 	public UserService()
 	{
 	}
+	public json register(String email,String password)
+    {
+    }
+	public json deleteUser(User user) 
+	{
+	}
+	public json logIn(String email,String password)
+    {
+    }
+	public json logOut()
+    {
+    }
+	public json setPassword(User user,String old,String new)
+	{
+	}
+	public json setEmail(User user,String new)
+	{
+	}
+
+
 }
