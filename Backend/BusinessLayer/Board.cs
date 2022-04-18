0bf942484d8a1e52035525b6c9496646d5e5caea
kafire135 <kfirniss@post.bgu.ac.il>
2022-04-18 10:02:01 +0300

    add task class and board class

---
+++ Backend/BusinessLayer/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    internal class Board
+    {
+        public string Title { get; set; }
+        public LinkedList<Task> Backlog { get; set; }
+        public LinkedList<Task> InProgress { get; set; }
+        public LinkedList<Task> Done { get; set; }
+
+        public LinkedList<Task> getTaskByType(Enum type) { return null; }
+        public void addTask(String title, Date duedate, String description) { }
+        public void removeTask(String title) { }
+        public void advanceTask(String title) { }
+        public Task searchTask(String title) { return null; }
+
+    }
+}
---
+++ Backend/BusinessLayer/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+   
+    internal class Task
+    {
+        public string Title { get; set; }
+        public Date CreationTime { get; set; }
+        public Date DueDate { get; set; }
+        public string Description { get; set; }
+        public Enum State { get; set; }
+        public Boolean DescriptionCharCap { get; set; }
+        public int DESCRIPTION_CHAR_CAP = 300;
+    }
+}
