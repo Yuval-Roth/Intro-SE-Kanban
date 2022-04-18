d226804bf097ce8ee14431a25e53c97f25c9d5f7
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-04-18 10:15:10 +0300

    Added BinaryTree.cs, BoardTree.cs, Date.cs

---
+++ Backend/BusinessLayer/BoardTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    internal class BoardTree<T> : BinaryTree<T> 
+    {
+       
+        public BoardTree() 
+        {
+        }
+        public LinkedList<Board> getAllBoards(User user)
+        {
+        }
+        public void addBoard(User user, Board toAdd)
+        { 
+        }
+        public void removeBard(User user, Board toRemove)
+        { 
+        }
+        private class BoardTreeNode : BinaryTreeNode<>
+        {
+            internal BoardTreeNode()
+            {
+            }
+            internal LinkedList<Board> getAllBoards(User user)
+            {
+            }
+            internal void addBoard(User user, Board toAdd)
+            {
+            }
+            internal void removeBard(User user, Board toRemove)
+            {
+            }
+        }
+    }
+}
