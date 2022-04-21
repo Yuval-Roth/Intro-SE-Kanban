969199f33d3302b6deff31dbfffd5af832203245
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-04-21 15:45:38 +0300

    Updated the unit tests

--- Backend/ServiceLayer/BoardsService.cs
+++ Backend/ServiceLayer/BoardsService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace IntroSE.Kanban.Backend.ServiceLayer
 {
-    internal class BoardsService
+    public class BoardsService
     {
 
         //==================================================
