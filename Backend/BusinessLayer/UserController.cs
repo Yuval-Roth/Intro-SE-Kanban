9868613d9e323a1ab501149449bbef7aebb55a83
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2022-04-21 09:05:57 +0300

    changes

--- Backend/BusinessLayer/UserController.cs
+++ Backend/BusinessLayer/UserController.cs
@@ -23,22 +23,26 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             if(password == null){ throw new ArgumentException ("ilegal password"); }
             if (!IsLegalPassword(password)) { throw new ArgumentException("Password ilegal"); }
             User newUser = new User(email, password);
-            if (userList.Contains(newUser))
+            try
             {
-                throw new ArgumentException("A user with that email already exists in the system");
+                userList.Add(newUser);
             }
-            userList.Add(newUser);
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            
         }
         public void DeleteUser(User user)
         {
-            if(user == null)  throw new ArgumentNullException("input is null"); 
-            if (userList.Contains(user))
+            if(user == null)  throw new ArgumentNullException("input is null");
+            try
             {
                 userList.Remove(user);
             }
-            else
+            catch (NoSuchElementException)
             {
-                throw new ArgumentException("there is no such user in the system");
+                throw;
             }
         }
         public void LogIn(String email, String password)
@@ -50,11 +54,15 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             {
                 if (newUser.CheckPasswordMatch(password))
                 {
-                    loggedIn.Add(email, newUser);
-                }
-                else
-                {
-                    throw new ArgumentException("wrong password");
+                    try
+                    {
+                        loggedIn.Add(email, newUser);
+                    }
+                    catch(ArgumentException)
+                    {
+                        throw;
+                    }
+                    
                 }
             }
             else
@@ -84,8 +92,6 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             {
                 throw new ArgumentException("password incorrect");
             }
-            
-
         }
 
         public void SetEmail(User user, String newE)
@@ -125,39 +131,6 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             }
 
             return false;
-
-
-            //if (pass == null) { return false; }
-            //if (pass.Length < MIN_PASS_LENGTH | pass.Length > MAX_PASS_LENGTH) { return false; }
-            //String SmallLet = @"\b[a-z]{1,}";
-            //String BigLet = @"\b[A-Z]{1,}";
-            //String num = @"\b[0-9]{1,}";
-            //Regex rg = new Regex(@SmallLet); 
-            //MatchCollection matchedAuthors = rg.Matches(pass);
-            //for (int i = 0; i < matchedAuthors.Count; i++)
-            //{
-            //    if (matchedAuthors[i].Equals(pass))
-            //    {
-            //        Regex rg2 = new Regex(BigLet);
-            //        MatchCollection matchedAuthors2 = rg2.Matches(pass);
-            //        for(int j = 0; j < matchedAuthors2.Count; j++)
-            //        {
-            //            if (matchedAuthors2[j].Equals(pass))
-            //            {
-            //                Regex rg3 = new Regex(num);
-            //                MatchCollection matchedAuthors3 = rg3.Matches(pass);
-            //                for(int k = 0; k < matchedAuthors3.Count; k++)
-            //                {
-            //                    if (matchedAuthors3[k].Equals(pass))
-            //                    {
-            //                        return true;
-            //                    }
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-            //return false;
         }
     }
 }
