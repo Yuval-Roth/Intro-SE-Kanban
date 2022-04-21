f77bb558b696f124dc3c14a05cf64c8469ebce73
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2022-04-21 11:03:14 +0300

    add test classes

---
+++ BackendTests/ServiceLayer/BoardControllerServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer.Tests
+{
+    [TestClass()]
+    public class BoardControllerServiceTests
+    {
+        [TestMethod()]
+        public void BoardControllerServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void createBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void deleteBoardTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getAllTasksByStateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getAllBoardsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void searchBoardTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ BackendTests/ServiceLayer/TaskServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer.Tests
+{
+    [TestClass()]
+    public class TaskServiceTests
+    {
+        [TestMethod()]
+        public void TaskServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setTitleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getDuedateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setDuedateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getDescriptionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setDescriptionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getStateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setStateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
