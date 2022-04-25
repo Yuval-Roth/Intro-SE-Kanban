ffbf25b506a684215094e91c8a0b582f9e360893
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2022-04-25 13:07:17 +0300

    Updates to UserData

--- Backend/BusinessLayer/UserData.cs
+++ Backend/BusinessLayer/UserData.cs
@@ -38,25 +38,138 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         }
         private BinaryTree<string, DataUnit> tree;
 
-        /// <summary>
-        /// Creates an empty <c>BoardTree</c>
-        /// </summary>
         public UserData()
         {
             tree = new BinaryTree<string, DataUnit>();
         }
+
         public User SearchUser(string email)
         {
-            return tree.GetData(email).User;
+            try
+            {
+                return tree.GetData(email).User;
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }
+            
         }
         public void AddUser(string email)
         {
-            
-        
+            try
+            {
+                tree.Add(email, new DataUnit());
+            }
+            catch (ArgumentException) 
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }        
         }
-        public LinkedList<Board> GetAllBoards(string email)
+        public void RemoveUser(string email) 
         {
-            return tree.GetData(email).boards;
+            try
+            {
+                tree.Remove(email);
+            }
+            catch (NoSuchElementException) 
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }
+        }
+
+        /// <summary>
+        /// Gets all the <c>User</c>'s <c>Board</c>s
+        /// <br/><br/>
+        /// <b>Throws</b> <c>NoSuchElementException</c> if the <c>User</c> does not exist<br/>
+        /// in the system
+        /// </summary>
+        /// <returns><c>LinkedList</c> of type <c>Board</c></returns>
+        /// <exception cref="NoSuchElementException"></exception>
+        public LinkedList<Board> GetBoards(string email)
+        {
+            try
+            {
+                return tree.GetData(email).boards;
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }
+        }
+
+        /// <summary>
+        /// Adds a <c>Board</c> to the <c>User</c>.
+        ///<br/><br/>
+        ///<b>Throws</b> <c>ArgumentException</c> if a <c>Board</c> with that title already exists <br/>
+        /// for the <c>User</c><br/><br/>
+        /// <b>Throws</b> <c>NoSuchElementException</c> if the <c>User</c> doesn't exist <br/>
+        /// in the system
+        /// </summary>
+        /// <returns>The <c>Board</c> that was added</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NoSuchElementException"></exception>
+        public Board AddBoard(string email,string title) 
+        {
+            try
+            {
+                LinkedList<Board> boardList = tree.GetData(email).boards;
+                foreach (Board board in boardList) 
+                {
+                    if (board.Title == title)
+                        throw new ArgumentException("A board titled " +
+                            title + " already exists for the user with the email " + email);                
+                }
+                Board toReturn = new(title);
+                boardList.AddLast(toReturn);
+                return toReturn;
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }
+        }
+
+        /// <summary>
+        /// Removes a <c>User</c>'s <c>Board</c>
+        /// <br/><br/>
+        /// <b>Throws</b> <c>ArgumentException</c> if a <c>Board</c> with that title <br/>
+        /// doesn't exist for the user<br/><br/>
+        /// <b>Throws</b> <c>NoSuchElementException</c> if the <c>User</c> doesn't exist <br/>
+        /// in the system
+        /// </summary>
+        /// <exception cref="ArgumentException"></exception>
+        /// /// <exception cref="NoSuchElementException"></exception>
+        public void RemoveBoard(string email, string title)
+        {
+            try
+            {
+                bool found = false;
+                LinkedList<Board> boardList = tree.GetData(email).boards;
+                foreach (Board board in boardList)
+                {
+                    if (board.Title == title) 
+                    {
+                        boardList.Remove(board);
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (! found)
+                    throw new ArgumentException("A board titled " +
+                                title + " doesn't exists for the user with the email " + email);
+            }
+            catch (NoSuchElementException)
+            {
+                throw new NoSuchElementException("A user with the email '" +
+                    email + "' doesn't exist in the system");
+            }
         }
     }
 }
