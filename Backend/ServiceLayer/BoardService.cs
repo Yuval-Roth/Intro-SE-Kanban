7e8ed681eed69f88c1d281b8b9d4ab7f684c6dbf
kafire135 <kfirniss@post.bgu.ac.il>
2022-04-27 00:16:55 +0300

    add BoardService documention

--- Backend/ServiceLayer/BoardService.cs
+++ Backend/ServiceLayer/BoardService.cs
@@ -6,6 +6,26 @@ using System.Threading.Tasks;
 
 namespace IntroSE.Kanban.Backend.ServiceLayer
 {
+
+    /// <summary>
+	///This class implements UserService 
+	///<br/>
+	///<code>Supported operations:</code>
+	///<br/>
+	/// <list type="bullet">AddTask()</list>
+	/// <list type="bullet">RemoveTask()</list>
+	/// <list type="bullet">LimitColumn()</list>
+    /// /// <list type="bullet">GetColumnLimit()</list>
+	/// <list type="bullet">GetColumnName()</list>
+	/// <list type="bullet">GetColumn()</list>
+	/// <br/><br/>
+	/// ===================
+	/// <br/>
+	/// <c>Ⓒ Kfir Nissim</c>
+	/// <br/>
+	/// ===================
+	/// </summary>
+    /// 
     public class BoardService
     {
         private readonly BusinessLayer.BoardController boardController;
@@ -15,26 +35,76 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
             boardController = new(userData);
         }
 
+        /// <summary>
+        /// This method adds a new task.
+        /// </summary>
+        /// <param name="email">Email of the user. The user must be logged in.</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="title">Title of the new task</param>
+        /// <param name="description">Description of the new task</param>
+        /// <param name="dueDate">The due date if the new task</param>
+        /// <returns>Response with user-email, unless an error occurs (see <see cref="BoardService"/>)</returns>
         public string AddTask(string email, string boardName, string title, string description, DateTime dueDate)
         {
             return "";
         }
-        public string RemoveTask(string email, string boardTitle, string title)
+
+        /// <summary>
+        /// This method removes a task to the specific user and board.
+        /// </summary>
+        /// <param name="email">Email of the user. The user must be logged in.</param>
+        /// <param name="boardTitle">The name of the board</param>
+        /// <param name="taskID">id of the task</param>
+        /// <returns>The string "{}", unless an error occurs (see <see cref="BoardService"/>)</returns>
+        public string RemoveTask(string email, string boardTitle, int taskID)
         {
             return "";
         }
+
+        /// <summary>
+        /// This method limits the number of tasks in a specific column.
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <param name="limit">The new limit value. A value of -1 indicates no limit.</param>
+        /// <returns>The string "{}", unless an error occurs (see <see cref="BoardService"/>)</returns>
         public string LimitColumn(string email, string boardName, int columnOrdinal, int limit)
         {
             return "";
         }
+
+        /// <summary>
+        /// This method gets the limit of a specific column.
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>Response with column limit value, unless an error occurs (see <see cref="BoardService"/>)</returns>
         public string GetColumnLimit(string email, string boardName, int columnOrdinal)
         {
             return "";
         }
+
+        /// <summary>
+        /// This method gets the name of a specific column
+        /// </summary>
+        /// <param name="email">The email address of the user, must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>Response with column name value, unless an error occurs (see <see cref="BoardService"/>)</returns>
         public string GetColumnName(string email, string boardName, int columnOrdinal)
         {
             return "";
         }
+
+        /// <summary>
+        /// This method returns a column given it's name
+        /// </summary>
+        /// <param name="email">Email of the user. Must be logged in</param>
+        /// <param name="boardName">The name of the board</param>
+        /// <param name="columnOrdinal">The column ID. The first column is identified by 0, the ID increases by 1 for each column</param>
+        /// <returns>Response with  a list of the column's tasks, unless an error occurs (see <see cref="BoardService"/>)</returns>
         public string GetColumn(string email, string boardName, int columnOrdinal)
         {
             return "";
