98ad9a84dfba25a4dd222358bd533a9a5bc0c78d
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-05-18 14:06:01 +0300

    rewrote code in Board

--- Backend/BusinessLayer/Board.cs
+++ Backend/BusinessLayer/Board.cs
@@ -91,19 +91,17 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
             }
             else 
             {
-                log.Error("AddTask() failed: board '" + this.title + "' has reached its limit and can't contain more tasks");
-                throw new ArgumentException("A board titled " +
-                        this.title + " has a limit and can't contains more task");
+                log.Error("AddTask() failed: Backlog in board '" + this.title + "' has reached its limit and can't contain more tasks");
+                throw new ArgumentException("Backlog in board '" + this.title + "' has reached its limit and can't contain more tasks");
             }
         }
 
-
         /// <summary>
         /// Remove <c>Task</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>Exception</c> if the task isn't exist
+        /// <b>Throws</b> <c>NoSuchElementException</c> if the task doesn't exist
         /// </summary>
         /// <param name="taskID"></param>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NoSuchElementException"></exception>
         public void RemoveTask(int taskId)
         {
             log.Debug("RemoveTask() taskId: " + taskId);
@@ -138,10 +136,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// </summary>
         /// <param name="taskId"></param>
         /// <param name="columnOrdinal"></param>
+        /// <exception cref="NoSuchElementException"></exception>
         /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void AdvanceTask(int columnOrdinal, int taskId)
         {
-            log.Debug("AdvanceTask() for column and taskId: " + (TaskStates)columnOrdinal + ", " + taskId);
+            log.Debug("AdvanceTask() for column and taskId: " + columnOrdinal + ", " + taskId);
             ValidateColumnOrdinal(columnOrdinal);
 
             if (taskStateTracker.ContainsKey(taskId))
@@ -186,7 +186,7 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// </summary>
         /// <param name="taskId"></param>
         /// <param name="columnOrdinal"></param>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NoSuchElementException"></exception>
         /// <exception cref="IndexOutOfRangeException"></exception>
         public Task SearchTask(int taskId, int columnOrdinal)
         {
@@ -214,7 +214,7 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
         /// </summary>
         /// <param name="taskId"></param>
         /// <returns>Task, unless an error occurs</returns>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NoSuchElementException"></exception>
         public Task SearchTask(int taskId) 
         {
             log.Debug("SearchTask() taskId: " + taskId);
@@ -249,74 +249,60 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 
         /// <summary>
         /// Get<c>column limit</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>ArgumentException</c> if the column doesn't have a limit <br/>
         /// <b>Throws</b> <c>IndexOutOfRangeException</c> if the column is not a valid column number
         /// </summary>
         /// <param name="columnOrdinal"></param>
         /// <returns>int column limit, unless an error occurs</returns>
-        /// <exception cref="ArgumentException"></exception>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public int GetColumnLimit(int columnOrdinal)
         {
             log.Debug("GetColumnLimit() columnOrdinal: " + columnOrdinal);
             ValidateColumnOrdinal(columnOrdinal);
 
-            if (columnLimit[columnOrdinal] != -1)
-            {
-                log.Debug("GetColumnLimit() success");
-                return columnLimit[columnOrdinal];
-            }
-            else 
-            {
-                log.Error("GetColumnLimit() failed: '" + (TaskStates)columnOrdinal + "' has no limit");
-                throw new ArgumentException("the column '" +
-                    (TaskStates)columnOrdinal + "' has no limit");
-            }
-            //else
+            log.Debug("GetColumnLimit() success");
+            return columnLimit[columnOrdinal];
+
+            //if (columnLimit[columnOrdinal] != -1)
             //{
-            //    return -1;
+            //    log.Debug("GetColumnLimit() success");
+            //    return columnLimit[columnOrdinal];
             //}
-            
+            //else 
+            //{
+            //    log.Error("GetColumnLimit() failed: '" + (TaskStates)columnOrdinal + "' has no limit");
+            //    throw new ArgumentException("the column '" +
+            //        (TaskStates)columnOrdinal + "' has no limit");
+            //}
         }
 
 
         /// <summary>
         /// Get<c>column name</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>Exception</c> if column isn't exist
+        /// <b>Throws</b> <c>IndexOutOfRangeException</c> if the column is not a valid column number
         /// </summary>
         /// <param name="columnOrdinal"></param>
         /// <returns>column name, unless an error occurs</returns>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public string GetColumnName(int columnOrdinal)
         {
-            log.Debug("GetColumnName() columnOrdinal: " + (TaskStates)columnOrdinal);
-            if (columnOrdinal < (int)TaskStates.backlog || columnOrdinal > (int)TaskStates.done)
-            {
-                log.Error("GetColumnName() failed: '" + columnOrdinal + "' doesn't exist");
-                throw new NoSuchElementException("A column '" +
-                    columnOrdinal + "' doesn't exist in the Board");
-            }
+            log.Debug("GetColumnName() columnOrdinal: " + columnOrdinal);
+            ValidateColumnOrdinal(columnOrdinal);
             
             log.Debug("GetColumnName() success");
-            return ((TaskStates) columnOrdinal).ToString();
+            return ((TaskStates)columnOrdinal).ToString();
         }
 
         /// <summary>
         /// Get<c>column</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>Exception</c> if column isn't exist
+        /// <b>Throws</b> <c>IndexOutOfRangeException</c> if the column is not a valid column number
         /// </summary>
         /// <param name="columnOrdinal"></param>
         /// <returns>LinkedList of task, unless an error occurs</returns>
-        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public LinkedList<Task> GetColumn(int columnOrdinal)
         {
-            log.Debug("GetColumn() columnOrdinal: " + (TaskStates)columnOrdinal);
-            if (columnOrdinal < (int)TaskStates.backlog || columnOrdinal > (int)TaskStates.done)
-            {
-                log.Error("GetColumn() failed: '" + columnOrdinal + "' doesn't exist");
-                throw new NoSuchElementException("A column '" +
-                    columnOrdinal + "' doesn't exist in the Board");
-            }
+            log.Debug("GetColumn() columnOrdinal: " + columnOrdinal);
+            ValidateColumnOrdinal(columnOrdinal);
 
             log.Debug("GetColumn() success");
             return columns[columnOrdinal];
@@ -324,60 +310,47 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 
         /// <summary>
         /// Limit<c>column</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>Exception</c> if column isn't exist or limit is illegal or column is over thr limit
+        /// <b>Throws</b> <c>ArgumentException</c> if column size is over the specified limit or the limit is invalid<br/>
+        /// <b>Throws</b> <c>IndexOutOfRangeException</c> if the column is not a valid column number
         /// </summary>
         /// <param name="columnOrdinal"></param>
         /// <param name="limit"></param>
         /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
         public void LimitColumn(int columnOrdinal, int limit)
         {
-            log.Debug("LimitColumn() for column and limit: " + (TaskStates)columnOrdinal + ", " + limit);
-            if (limit < 0)
+            log.Debug("LimitColumn() for column and limit: " + columnOrdinal + ", " + limit);
+            ValidateColumnOrdinal(columnOrdinal);
+
+            if (limit >= -1)
             {
-                log.Error("LimitColumn() failed: '" + limit + "' the limit is negative");
-                throw new NoSuchElementException("A limit '" +
-                    limit + "' is negative");
+                if (limit != -1 & columns[columnOrdinal].Count > limit)
+                {
+                    log.Error("LimitColumn() failed: '" + (TaskStates)columnOrdinal + "' size is bigger than the limit " + limit);
+                    throw new ArgumentException("A column '" +
+                        (TaskStates)columnOrdinal + "' size is bigger than th limit " + limit);
+                }
+                columnLimit[columnOrdinal] = limit;
+                log.Debug("LimitColumn() success");
             }
-            if (columnOrdinal < (int)TaskStates.backlog || columnOrdinal > (int)TaskStates.done)
+            else 
             {
-                log.Error("LimitColumn() failed: '" + columnOrdinal + "' doesn't exist");
-                throw new NoSuchElementException("A column '" +
-                    columnOrdinal + "' doesn't exist in the Board");
-            }
-            if (columns[columnOrdinal].Count > limit)
-            {
-                log.Error("LimitColumn() failed: '" + (TaskStates)columnOrdinal + "' size is bigger than th limit " +limit);
-                throw new NoSuchElementException("A column '" +
-                    (TaskStates)columnOrdinal + "' size is bigger than th limit " + limit);
-            }
-            log.Debug("LimitColumn() success");
-            columnLimit[columnOrdinal] = limit;
+                log.Error("LimitColumn() failed: '" + limit + "' the limit is not valid");
+                throw new ArgumentException("A limit '" +
+                    limit + "' is not valid");
+            } 
         }
 
         /// <summary>
-        /// Unlimit<c>column</c> from <c>Board</c> board <br/> <br/>
-        /// <b>Throws</b> <c>Exception</c> if column isn't exist
+        /// <b>Throws</b> <c>IndexOutOfRangeException</c> if the column is not a valid column number
         /// </summary>
         /// <param name="columnOrdinal"></param>
-        /// <exception cref="ArgumentException"></exception>
-        public void UnlimitColumn(int columnOrdinal)
-        {
-            log.Debug("UnimitColumn() for column: " + (TaskStates)columnOrdinal);
-            if (columnOrdinal < (int)TaskStates.backlog || columnOrdinal > (int)TaskStates.done)
-            {
-                log.Error("LimitColumn() failed: '" + columnOrdinal + "' doesn't exist");
-                throw new NoSuchElementException("A column '" +
-                    columnOrdinal + "' doesn't exist in the Board");
-            }
-            log.Debug("LimitColumn() success");
-            columnLimit[columnOrdinal] = -1;
-        }
-
+        /// <exception cref="IndexOutOfRangeException"></exception>
         private void ValidateColumnOrdinal(int columnOrdinal)
         {
             if (columnOrdinal < (int)TaskStates.backlog | columnOrdinal > (int)TaskStates.done)
             {
-                log.Error("AdvanceTask() failed: '" + columnOrdinal + "' is not a valid column number");
+                log.Error("ValidateColumnOrdinal() failed: '" + columnOrdinal + "' is not a valid column number");
                 throw new IndexOutOfRangeException("The column '" + columnOrdinal + "' is not a valid column number");
             }
         }
@@ -388,29 +361,22 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 
         public Serializable.Board_Serializable GetSerializableInstance() 
         {
-            LinkedList<Serializable.Task_Serializable> serializableBackLog = new();
-            foreach (Task task in columns[(int) TaskStates.backlog])
-            {
-                serializableBackLog.AddLast(task.GetSerializableInstance());
-            }
+            LinkedList<Serializable.Task_Serializable>[] serializableColumns = new LinkedList<Serializable.Task_Serializable>[3];
 
-            LinkedList<Serializable.Task_Serializable> serializableInProgress = new();
-            foreach (Task task in columns[(int)TaskStates.inprogress])
+            for (int column = (int)TaskStates.backlog ; column <= (int)TaskStates.done; column++)
             {
-                serializableInProgress.AddLast(task.GetSerializableInstance());
+                serializableColumns[column] = new LinkedList<Serializable.Task_Serializable>();
+                foreach (Task task in columns[(int)TaskStates.backlog])
+                {
+                    serializableColumns[column].AddLast(task.GetSerializableInstance());
+                }
             }
+            
 
-            LinkedList<Serializable.Task_Serializable> serializableDone = new();
-            foreach (Task task in columns[(int)TaskStates.done])
-            {
-                serializableDone.AddLast(task.GetSerializableInstance());
-            }
             return new Serializable.Board_Serializable()
             {
                 Title = title,
-                Backlog = serializableBackLog,
-                InProgress = serializableInProgress,
-                Done = serializableDone,
+                
             };
         }
     }
