06e36a62680aa4b72fa47f1637db107b451614c0
kafire135 <kfirniss@post.bgu.ac.il>
2022-06-02 23:12:28 +0300

    add boardControllerDTO

--- Backend/DataAccessLayer/BoardControllerDTO.cs
+++ Backend/DataAccessLayer/BoardControllerDTO.cs
@@ -8,5 +8,40 @@ namespace IntroSE.Kanban.Backend.DataAccessLayer
 {
     public class BoardControllerDTO
     {
+        private SQLExecuter executer;
+
+        public bool AddBoard(BoardDTO board)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool RemoveBoard(int id)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool JoinBoard(string email, int id)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool LeaveBoard(string email, int id)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool ChangeOwner(string email, int id)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool AddTask(int id, TaskDTO task)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool RemoveTask(int BoardId, int TaskId)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool LimitColumn(int Id, BusinessLayer.TaskStates state, int limit)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+
     }
 }
