d3c6398811b3457b9012beeb81ebd06df28101a4
kafire135 <kfirniss@post.bgu.ac.il>
2022-06-02 23:02:03 +0300

    add userControllerDTO

--- Backend/DataAccessLayer/UserControllerDTO.cs
+++ Backend/DataAccessLayer/UserControllerDTO.cs
@@ -8,5 +8,23 @@ namespace IntroSE.Kanban.Backend.DataAccessLayer
 {
     public class UserControllerDTO
     {
+        private SQLExecuter executer;
+
+        public bool AddUser(UserDTO user)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool DeleteUser(string email)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool ChangePassword(string email, string password)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
+        public bool ChangeEmail(string oldEmail, string newEmail)
+        {
+            throw new NotImplementedException("No implement yet");
+        }
     }
 }
