96559a94104ba94da567d5653fc8698207daf43b
Yuval Roth <Yuval-Roth@users.noreply.github.com>
2022-06-06 20:16:23 +0300

    small updates

--- Backend/DataAccessLayer/TaskDTO.cs
+++ Backend/DataAccessLayer/TaskDTO.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace IntroSE.Kanban.Backend.DataAccessLayer
 {
+    public enum TaskStates 
+    {
+        backlog,
+        inprogress,
+        done
+    }
     public class TaskDTO
     {
-        public int BoardId { set; get; }
+        //public int BoardId { set; get; } 
+        
+        // BoardID is not needed here because
+        // TaskDTO will be held inside BoardDTO when loading the data
+
+
         public int TaskId { set; get; }
         public string Title { set; get; }
         public string Description { set; get; }
         public DateTime CreationTime { set; get; }
         public DateTime DueDate { set; get; }
-        public BusinessLayer.TaskStates State { set; get; }
+        public TaskStates State { set; get; }
         public string Assignee { set; get; }
     }
 }
