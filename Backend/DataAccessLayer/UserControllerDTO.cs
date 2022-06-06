f4f834e2dc092223e1a855adfdcf945fc9379abd
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2022-06-06 21:31:52 +0300

    edit UserControllerDTO

--- Backend/DataAccessLayer/UserControllerDTO.cs
+++ Backend/DataAccessLayer/UserControllerDTO.cs
@@ -12,18 +12,27 @@ namespace IntroSE.Kanban.Backend.DataAccessLayer
 
         public bool AddUser(UserDTO user)
         {
+            executer.Execute("INSERT INTO Users (Email,Password)" +
+                $"VALUES({ user.Email},{user.Password})");
             throw new NotImplementedException("No implement yet");
         }
         public bool DeleteUser(string email)
         {
+            executer.Execute($"DELETE FROM Users WHERE Email= {email}");
             throw new NotImplementedException("No implement yet");
         }
         public bool ChangePassword(string email, string password)
         {
+            executer.Execute("UPDATE Users" +
+                $"SET Password = {password}" +
+                $"WHERE Email = {email}");
             throw new NotImplementedException("No implement yet");
         }
         public bool ChangeEmail(string oldEmail, string newEmail)
         {
+            executer.Execute("UPDATE Users" +
+                $"SET Email = {newEmail}" +
+                $"WHERE Email = {oldEmail}");
             throw new NotImplementedException("No implement yet");
         }
     }
