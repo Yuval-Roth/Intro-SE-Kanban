a4313651556840f28db1f61c3d2278972a9efb85
kafire135 <kfirniss@post.bgu.ac.il>
2022-06-08 12:29:57 +0300

    add BoardMembersPermissions class

---
+++ Backend/BusinessLayer/BoardMembersPermissions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BusinessLayer
+{
+    public class BoardMembersPermissions
+    {
+
+        public static bool AddTask(string email, Board board)
+        {
+
+        }
+
+    }
+}
