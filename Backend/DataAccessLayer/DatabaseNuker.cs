44b49819ae9982cf26693c6bb289b34bb0345a24
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-06-09 20:06:10 +0300

    Initial implementation for DeleteData. Added DatabaseNuker class in DAL and added a table for holding the board id counter in the database

--- Backend/DataAccessLayer/DatabaseNuker.cs
+++ Backend/DataAccessLayer/DatabaseNuker.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SQLite;
 
 namespace IntroSE.Kanban.Backend.DataAccessLayer
 {
     public class DatabaseNuker
     {
+        private SQLExecuter executer;
+
+        public DatabaseNuker()
+        {
+            executer = DataAccessLayerFactory.GetInstance().SQLExecuter;
+        }
+
         public void Nuke()
         {
-            
+            string[] tables = { "Users", "Boards","UserJoinedBoards","Tasks"};
+            foreach (string table in tables)
+            {
+                executer.ExecuteWrite($"DELETE FROM {table}");
+            }
+            executer.ExecuteWrite($"UPDATE BoardIDCounter SET Counter = 0");
         }
     }
 }
