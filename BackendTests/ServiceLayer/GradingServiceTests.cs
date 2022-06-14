c405e9e7b69c4243d66c1889c30ca2e5e4414bdd
Yuval Roth <rothyuv@post.bgu.ac.il>
2022-06-14 15:37:18 +0300

    added GradingServiceTests

---
+++ BackendTests/ServiceLayer/GradingServiceTests.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroSE.Kanban.Backend.ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer.Tests
+{
+    [TestClass()]
+    public class GradingServiceTests
+    {
+        [TestMethod()]
+        public void GradingService_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Register_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Login_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Logout_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LimitColumn_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetColumnLimit_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetColumnName_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTask_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTaskDueDate_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTaskTitle_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTaskDescription_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AdvanceTask_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetColumn_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddBoard_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveBoard_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InProgressTasks_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserBoards_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void JoinBoard_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LeaveBoard_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AssignTask_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadData_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteData_Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TransferOwnership_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
