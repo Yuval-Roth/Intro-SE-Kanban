90c123017010dc2b1572092290dd48c4088cc245
hadasprintz <92999292+hadasprintz@users.noreply.github.com>
2022-06-26 22:16:29 +0300

    v

--- Frontend/ViewModel/BoardPageViewModel.cs
+++ Frontend/ViewModel/BoardPageViewModel.cs
@@ -36,6 +36,8 @@ namespace IntroSE.Kanban.Frontend.ViewModel
         public TextBox ChosenBoard => chosenBoard;
         public GridView BoardTable => boardTable;
 
+        
+
         public Button Submit => submit;
         
         private Model.BoardController boardController = new Model.BoardController();
